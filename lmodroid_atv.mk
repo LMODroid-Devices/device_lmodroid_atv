6e26350630366fa2ce80ef4f36e84e23453bd287
Thomas Makin <halorocker89@gmail.com>
2024-02-02 13:38:25 -0500

    atv: init: Allow opt-out of forced 1080p scaling
    * This has not been mandated by Google since S and since they fixed
      density scaling, UI looks better with properly scaling interface.
      As a backup for targets that still desire 1080p scaling, we introduce
      a new flag TARGET_ATV_FORCE_1080_SCALING to use the replacement
      init-script. This flag is set by default for compat.
    
    Change-Id: Id1eb10b3f0c2b03ac6401bdfb00ba68036de91ef

--- lmodroid_atv.mk
+++ lmodroid_atv.mk
@@ -26,6 +26,11 @@ PRODUCT_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     init.lmodroid.atv.rc
 
+ifneq ($(TARGET_ATV_FORCE_1080_SCALING),false)
+PRODUCT_PACKAGES += \
+    init.lmodroid.atv.scaling.rc
+endif
+
 # Dynalink 4k
 PRODUCT_PACKAGES += \
     Vendor_0110_Product_0508.idc \
