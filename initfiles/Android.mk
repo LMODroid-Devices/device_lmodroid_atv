LineageOS/android_device_lineage_atv

atv: init: Allow opt-out of forced 1080p scaling
* This has not been mandated by Google since S and since they fixed
  density scaling, UI looks better with properly scaling interface.
  As a backup for targets that still desire 1080p scaling, we introduce
  a new flag TARGET_ATV_FORCE_1080_SCALING to use the replacement
  init-script. This flag is set by default for compat.

Change-Id: Id1eb10b3f0c2b03ac6401bdfb00ba68036de91ef

initfiles/Android.mk
@@ -7,3 +7,11 @@ LOCAL_SRC_FILES            := init.lineage.atv.rc
 LOCAL_MODULE_RELATIVE_PATH := init
 LOCAL_PRODUCT_MODULE       := true
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE               := init.lineage.atv.scaling.rc
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := init.lineage.atv.scaling.rc
+LOCAL_MODULE_RELATIVE_PATH := init
+LOCAL_PRODUCT_MODULE       := true
+include $(BUILD_PREBUILT)

LMODroid-Devices/device_lmodroid_atv

initfiles/Android.mk
@@ -7,3 +7,11 @@ LOCAL_SRC_FILES            := init.lmodroid.atv.rc
 LOCAL_MODULE_RELATIVE_PATH := init
 LOCAL_PRODUCT_MODULE       := true
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE               := init.lmodroid.atv.scaling.rc
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := init.lmodroid.atv.scaling.rc
+LOCAL_MODULE_RELATIVE_PATH := init
+LOCAL_PRODUCT_MODULE       := true
+include $(BUILD_PREBUILT)