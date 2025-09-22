facd02e941c1bf91ebf51b3cd186314ed6f8d757
Mohammad Hasan Keramat J <ikeramat@libremobileos.com>
2025-09-22 09:10:37 +0330

    atv: Adapt to LMODroid
    Change-Id: I87d1b5390f3de1230335ad0c38dd9514b3fd1b44

--- lmodroid_atv.mk
+++ lmodroid_atv.mk
@@ -8,7 +8,7 @@ include $(LOCAL_PATH)/system_prop.mk
 
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += \
-    device/lineage/atv/overlay
+    device/lmodroid/atv/overlay
 
 # Init files
 PRODUCT_PACKAGES += \
@@ -114,7 +114,7 @@ PRODUCT_PACKAGES += \
 
 # priv-app permissions
 PRODUCT_COPY_FILES +=\
-    device/lineage/atv/permissions/privapp-permissions-lineage-atv.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-lineage-atv.xml
+    device/lmodroid/atv/permissions/privapp-permissions-lineage-atv.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-lineage-atv.xml
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
